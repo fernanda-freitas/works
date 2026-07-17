cfb16e5521924ac447656ab73bbd2213b7674a52
Fernanda Freitas <idea.fernanda@gmail.com>
2026-07-17 08:03:48 +0100

    works

--- app/work/page.js
+++ app/work/page.js
@@ -1,11 +1,11 @@
 
 export default function Works() {
     return(
-        <h1 className="container-fill-text">
-            <span className="container-fill-text__container">
-                <span className="container-fill-text__display">Coffee community.</span>
+        <h1 className="container-fill-text font-normal">
+            <span className="container-fill-text__container text-trim">
+                <span className="container-fill-text__display">Works</span>
             </span>
-            <span className="container-fill-text__reference" aria-hidden="true">Coffee community.</span>
+            <span className="container-fill-text__reference" aria-hidden="true">Works</span>
         </h1>
     )
 }
